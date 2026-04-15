61eee20086ce686ac6db7fc8d448de56503f98e2
Adam Pietrasiak <adam@pietrasiak.com>
2026-04-15 12:35:57 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,7 @@ const Footer = styled.a`
   position: fixed;
   bottom: 10px;
   right: 14px;
-  font-size: 10px;
+  font-size: 16px;
   font-weight: 500;
   color: rgba(255, 255, 255, 0.3);
   text-decoration: none;
